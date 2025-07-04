2c3664de4fd7efe941ead40886213781305a8401
William Bruntrager <w.bruntrager@gmail.com>
2025-07-04 10:39:35 +0200

    Add ecosystem file

---
+++ ecosystem.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  apps: [
+    {
+      name: 'homophone-server',
+      script: 'server.js',
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '256M',
+      env: {
+        NODE_ENV: 'development',
+        PORT: 11001
+      },
+      error_file: './logs/err.log',
+      out_file: './logs/out.log',
+      log_file: './logs/combined.log',
+      time: true
+    }
+  ]
+}
